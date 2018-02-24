e21a07e6c8aabb54166a2168d93a34387425c203
Sara Hanberg <sarahanberg69@gmail.com>
2018-02-23 18:56:15 -0800

    Add files via upload

--- Pizza Project/js/pizzajava.js
+++ Pizza Project/js/pizzajava.js
@@ -1,24 +1,31 @@
-/*saving this info as possible things for later*/
 
-var size_prices = new Array();
-pizza_prices["personal"]=6;
-pizza_prices["medium"]=10;
-pizza_prices["large"]=14;
-pizza_prices["xlarge"]=16;
+function valueAndCost() {
+	var runningTotal = 0,
+        sizeTotal = 0;
+    select = "<h3>You Ordered:</h3>";   
+	price = "<h3>Item Price:</h3>";  
+    sizeArray = document.getElementsByClassName("size");
+	for (var i = 0; i < sizeArray.length; i++) {
+		if (sizeArray[i].checked) {
+			var selectedSize = sizeArray[i].value;
+			select = select+selectedSize+"<br>";
+		}
+	}
+	if (selectedSize === "personal") {
+		sizeTotal = 6;	
+	} else if (selectedSize === "medium") {
+		sizeTotal = 10;
+	} else if (selectedSize === "large") {
+		sizeTotal = 14;
+	} else if (selectedSize === "xlarge") {
+		sizeTotal = 16;
+	}
+	price = price+sizeTotal+"<br>";
+	runningTotal = sizeTotal;
+	getMeat(runningTotal,select,price); 
 
-
-function getPizzaSizePrice()
-{
-    var PizzaSizePrice=0;
-    var theForm = document.forms["orderform"];
-    var selectedSize = theForm.elements["size"];
-    for(var i = 0; i < selectedSize.length; i++)
-    {
-        if(selectedSize[i].checked)
-        {
-            PizzaSizePrice = pizza_prices[selectedSize[i].value];
-            break;
-        }
-    }
-    return PizzaSizePrice();
+    document.getElementById("portion").innerHTML=select;
+	document.getElementById("ItemPrice").innerHTML=price;
+	document.getElementById("Total").innerHTML = "<br><h3></br> Total Price:$"+runningTotal+".00"+"</h3>";
 }
+     
